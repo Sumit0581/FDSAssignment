4ae7d16d4e17f1b56ffa8f7fd4f820f327d4c3e3
Sumit0581 <sb03042000@gmail.com>
2021-04-04 19:48:07 +0530

    added fourier basis and modified dataset

--- eigensample.cpp
+++ eigensample.cpp
@@ -1,3 +1,4 @@
+#define _USE_MATH_DEFINES
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -15,13 +16,13 @@ using namespace std;
 using namespace Eigen;
 namespace plt = matplotlibcpp;
 
-MatrixXf computephi(int complexity, string model, vector<float> inp, vector<float> test={0,0}){
+MatrixXf computephi(int complexity, string model, vector<float> inp,float space =0.01, vector<float> test={0,0}){
 	MatrixXf phi(inp.size(),complexity);
 	if(model.compare("exp")==0){
 		for(int i=0;i<inp.size();i++){
 			phi(i,0) = 1;
 			for(int j=1;j<complexity;j++){
-			phi(i,j) = exp((-1.0/2.0)*(pow((inp[i]-test[j])/110,2)));
+			phi(i,j) = exp((-1.0/2.0)*(pow((inp[i]-test[j])/space,2)));
 			}
 		}
 	}
@@ -29,6 +30,27 @@ MatrixXf computephi(int complexity, string model, vector<float> inp, vector<floa
 		for(int i=0;i<inp.size();i++){
 			for(int j=0;j<complexity;j++){
 				phi(i,j) = pow(inp[i],j);
+			}
+		}
+	}
+	else if(model.compare("sigmoid")==0){
+		for(int i=0;i<inp.size();i++){
+			phi(i,0) = 1;
+			for(int j=1;j<complexity;j++){
+			phi(i,j) = 1.0/(1+exp((-1.0)*((inp[i]-test[j])/space)));
+			}
+		}
+	}
+	else if(model.compare("fourier")==0){
+		for(int i=0;i<inp.size();i++){
+			phi(i,0) = 1;
+			for(int j=1;j<complexity;j++){
+				if(j%2==0){
+					phi(i,j)=sin(0.4*M_PI*inp[i]*j);
+				}
+				else{
+					phi(i,j)=cos(0.4*M_PI*inp[i]*j);
+				}			
 			}
 		}
 	}
@@ -74,19 +96,15 @@ int main()
 	}
 	vector<float> testinp, traininp;
 	vector<float> testout,trainout;
-	vector<int> randnos(30,-1);
-	for(int i=0;i<30;i++){
-		int temp = rand()%150;
+	vector<int> randnos(150,-1);
+	for(int i=0;i<150;i++){
+		int temp = rand()%500;
 		while(find(randnos.begin(), randnos.end(), temp) != randnos.end()){
-			temp = rand()%150;
+			temp = rand()%500;
 		}
 		randnos[i] = temp;
 	}
 	sort(randnos.begin(),randnos.end());
-	// for(int i=0;i<50;i++){
-	// 	cout << randnos[i] << " " ;
-	// }
-	// cout << endl;
 	for(int i=0;i<inp.size();i++){
 		if(find(randnos.begin(), randnos.end(), i) != randnos.end()){
 			testinp.push_back(inp[i]);
@@ -99,34 +117,60 @@ int main()
 	}
 	cout << "Test size- Input: " << testinp.size() << " Output: " << testout.size() << endl;
 	cout << "Train size- Input: " << traininp.size() << " Output: " << trainout.size() << endl;
-	// for(int i=0;i<inp.size();i++){
-	// 	cout << i << " Temp: " << inp[i] << " Passengers: " << out[i] << endl;
-	// }
-	// cout << min << " " << max << endl;
 	vector<float> cntrs;
 	for(int i=0;i<inp.size();i++){
-		if(i%3==0) cntrs.push_back(inp[i]);
+		if(i%10==0) cntrs.push_back(inp[i]);
 	}
-	MatrixXf phi = computephi(cntrs.size(),"exp",traininp,cntrs);
+	float space=0.02;
+	// MatrixXf phi = computephi(cntrs.size(),"sigmoid",traininp,cntrs,space);
 	// MatrixXf phi = computephi(10,"poly",traininp);
+	MatrixXf phi = computephi(50,"fourier",traininp);
 	VectorXf y(trainout.size());
 	for(int i=0;i<trainout.size();i++){
 		y(i) = trainout[i];
 	}
+	//poly-10,0.000456,sigmoid-0.02,0.0004256,exp-0.1,0.000448
 	VectorXf weights = phi.bdcSvd(ComputeThinU | ComputeThinV).solve(y);
-	MatrixXf phi_test = computephi(cntrs.size(),"exp",inp,cntrs);
-	// MatrixXf phi_test = computephi(10,"poly",inp);
-	VectorXf model_out = phi_test*weights;
+	// MatrixXf phi_test = computephi(cntrs.size(),"sigmoid",testinp,cntrs,space);
+	// MatrixXf phi_train = computephi(cntrs.size(),"sigmoid",traininp,cntrs,space);
+	// MatrixXf phi_model = computephi(cntrs.size(),"sigmoid",inp,cntrs,space);
+	// MatrixXf phi_test = computephi(10,"poly",testinp);
+	// MatrixXf phi_train = computephi(10,"poly",traininp);
+	// MatrixXf phi_model = computephi(10,"poly",inp);
+	MatrixXf phi_test = computephi(50,"fourier",testinp);
+	MatrixXf phi_train = computephi(50,"fourier",traininp);
+	MatrixXf phi_model = computephi(50,"fourier",inp);
+	VectorXf model_out_test = phi_test*weights;
+	VectorXf model_out_train = phi_train*weights;
+	VectorXf model_out = phi_model*weights;
+	vector<long double> finalvaltest(testinp.size());
+	vector<long double> finalvaltrain(traininp.size());
 	vector<long double> finalval(inp.size());
+	for(int i=0;i<model_out_test.size();i++){
+		finalvaltest[i] = model_out_test(i);
+	}
+	for(int i=0;i<model_out_train.size();i++){
+		finalvaltrain[i] = model_out_train(i);
+	}
 	for(int i=0;i<model_out.size();i++){
 		finalval[i] = model_out(i);
 	}
-	long double loss = 0;
-	for(int i=0;i<model_out.size();i++){
-		loss += pow(finalval[i]-out[i],2);
+	long double train_loss = 0;
+	for(int i=0;i<model_out_train.size();i++){
+		train_loss += pow(finalvaltrain[i]-trainout[i],2);
 	}
-	cout << " Loss: " << loss << endl;
-	cout << testinp.size() << " " << finalval.size() << endl;
+	train_loss/=350;
+	long double test_loss = 0;
+	for(int i=0;i<model_out_test.size();i++){
+		test_loss += pow(finalvaltest[i]-testout[i],2);
+	}
+	test_loss/=150;
+	long double model_loss = 0;
+	for(int i=0;i<model_out.size();i++){
+		model_loss += pow(finalval[i]-out[i],2);
+	}
+	model_loss/=500;
+	cout << " Training Loss: " << train_loss <<  " Test Loss: " << test_loss << " Model Loss: " << model_loss << endl;
 	plt::plot(inp,out);
 	plt::plot(inp,finalval,"r--");
 	plt::show();
